d0304d774808ddaf4b15eb11f1053025f85a9a9f
sarahcastelar <sarahgcastelar@gmail.com>
2018-05-18 14:00:24 -0600

    agregue algo al main2

--- lab4_ArielPerez_SarahCastelar/src/lab4_arielperez_sarahcastelar/Jugador.java
+++ lab4_ArielPerez_SarahCastelar/src/lab4_arielperez_sarahcastelar/Jugador.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab4_arielperez_sarahcastelar;
 
-/**
- *
- * @author ariel
- */
+
 public class Jugador {
     
     private String nombre;
--- lab4_ArielPerez_SarahCastelar/src/lab4_arielperez_sarahcastelar/Lab4_ArielPerez_SarahCastelar.java
+++ lab4_ArielPerez_SarahCastelar/src/lab4_arielperez_sarahcastelar/Lab4_ArielPerez_SarahCastelar.java
@@ -1,21 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lab4_arielperez_sarahcastelar;
 
-/**
- *
- * @author Usuario
- */
+import javax.swing.JOptionPane;
+
+
 public class Lab4_ArielPerez_SarahCastelar {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int option = -1, option2 = -1;
+        
+        while (option != 3){
+            option = Integer.parseInt(JOptionPane.showInputDialog(
+                  "1. Crear creadores\n"
+                + "2. INICIO\n"
+                + "3. Salir\n"));
+            
+            
+            
+            if (option == 1) { //AGREGAR ELIMINAR Y LISTAR JUGADORES
+                
+                option2 = Integer.parseInt(JOptionPane.showInputDialog(
+                  "1. Agregar Nuevo Jugador\n"
+                + "2. Eliminar Juagador\n"
+                + "3. Enlistar Jugadores\n"));
+                
+                if (option2 == 1) {
+                    
+                }
+                if (option2 == 2) {
+                    
+                }
+                if (option2 == 3) {
+                    
+                }
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    
+    static void imprimeMatriz(char mat [][], int filas, int cols){
+        if (filas == mat.length-1 && cols == mat[0].length-1) {
+            System.out.println("[" + mat[filas][cols] + "]");
+            System.out.println();
+        } else {
+            if (cols == mat[0].length-1) {
+                System.out.println("[" + mat[filas][cols] + "]");
+                imprimeMatriz(mat, filas+1,0);
+            } else {
+                System.out.print("[" + mat[filas][cols] + "]");
+                imprimeMatriz(mat,filas,cols+1);
+            }
+        }
     }
     
 }
