1e342967209051a981a3428ad0810a90badc6c8c
sarahcastelar <sarahgcastelar@gmail.com>
2018-05-18 16:27:39 -0600

    probando

--- lab4_ArielPerez_SarahCastelar/src/lab4_arielperez_sarahcastelar/Caballeros.java
+++ lab4_ArielPerez_SarahCastelar/src/lab4_arielperez_sarahcastelar/Caballeros.java
@@ -17,8 +17,15 @@ public class Caballeros extends Pieza{
         String posMover [] = coordenadaMover.split(",");
         String xpp = posPieza[0], ypp = posPieza[1], xpm = posMover[0], ypm = posMover[1];
         int xposPieza = Integer.parseInt(xpp), yposPieza = Integer.parseInt(ypp), xposMover = Integer.parseInt(xpm), yposMover = Integer.parseInt(ypm);
+        Object [][] temporal = new Object [10][10];
         
-        System.out.println("HOLAAAAAAAAAAAAAAAAAA");
+        if ( ((xposPieza == xposMover && yposPieza-1 == yposMover) || (xposPieza == xposMover && yposPieza+1 == yposMover))
+                || (xposPieza-1 == xposMover && yposPieza == yposMover) || (xposPieza+1 == xposMover && yposPieza == yposMover) ){ //AQUI SE MUEVE VERTICAL HORIZONTAL.
+            
+            temporal[xposPieza][yposPieza] = mat[xposPieza][yposPieza];
+            mat[xposPieza][yposPieza] = null;
+            mat[xposMover][yposMover] = temporal[xposPieza][yposPieza];
+        }
         
         }
         
